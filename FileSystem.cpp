837f0c2537bfef4b5ee09328ead17a124db92321
Dimitar Stoyanov <bgrspot@gmail.com>
2020-07-16 17:38:24 +0300

    Added filesystem functions

--- FileSystem.cpp
+++ FileSystem.cpp
@@ -42,6 +42,118 @@ namespace detailEngine
 		return vectorCopy;
 	}
 
+	void FileSystem::LoadFile(std::string path) // loads a single file into the file list
+	{
+		// Lock the file list - try not to make deadlocks ok ? bye
+		// push back a file to the vector
+		// open the requested file and then access fileList.back() and add the opened file content to the <File> container
+		std::lock_guard<std::mutex> mut(fileioMutex);
+
+		files.push_back(File(UnixTimestamp()));
+	}
+
+	void FileSystem::LoadDir(std::string path) // loads all the file from a specific directory in the file list
+	{
+		std::lock_guard<std::mutex> mut(fileioMutex);
+
+	}
+
+	std::vector<std::string> FileSystem::DirGetAllFileNamesAbsolute(std::string path)
+	{
+		std::lock_guard<std::mutex> mut(fileioMutex);
+		std::vector<std::string> fileNames;
+
+
+
+		return fileNames;
+	}
+
+	std::string FileSystem::GetPathFileName(std::string path)
+	{
+		std::string fileName = GetPathFullFilename(path);
+		std::string out;
+		for (int i = 0; i < fileName.size(); ++i)
+		{
+			if (fileName[i] == '.')
+			{
+				if (i + 1 < fileName.size())
+				{
+					if (fileName[i + 1] != '.') // support for /../../
+						break;
+				}
+			}
+			out += fileName[i];
+		}
+		return out;
+	}
+
+	std::string FileSystem::GetPathFileType(std::string path)
+	{
+		std::string fileName = GetPathFullFilename(path);
+		std::string out;
+		bool begin = false;
+
+		for (int i = 0; i < fileName.size(); ++i)
+		{
+			if (fileName[i] == '.')
+				begin = true;
+
+			if (begin)
+			{
+				out += fileName[i];
+			}
+		}
+		return out;
+	}
+
+	std::string FileSystem::GetPathFullFilename(std::string path)
+	{
+		return SanitizePath(path).back();
+	}
+
+	std::string FileSystem::GetSanitizedPath(std::string path)
+	{
+		std::vector<std::string> words = SanitizePath(path);
+
+		std::string out = "";
+
+		for (std::string& str : words)
+		{
+			out += str + "/";
+		}
+
+		return out;
+	}
+
+	std::vector<std::string> FileSystem::SanitizePath(std::string path)
+	{
+		std::vector<std::string> out;
+		std::string filter = "";
+		bool reset = false;
+
+		for (int i = 0; i < path.size(); ++i)
+		{
+			if (path[i] == '\\' || path[i] == '/')
+			{
+				reset = true;
+			}
+			else
+			{
+				if (reset)
+				{
+					out.push_back(filter);
+					filter = "";
+					reset = false;
+				}
+				filter += path[i];
+			}
+		}
+		out.push_back(filter);
+
+		return out;
+
+	}
+
 	void FileSystem::ExecuteAllRequests()
 	{
 		SwapRequestBuffers();
--- FileSystem.hpp
+++ FileSystem.hpp
@@ -23,7 +23,12 @@ namespace detailEngine
 	class File 
 	{
 	public:
-		File() {}
+		File(long int currentTime) : creationTime(currentTime) {}
+
+		void SetName(std::string Name)
+		{
+			name = Name;
+		}
 
 		void Fill(std::ifstream& file)
 		{
@@ -47,7 +52,9 @@ namespace detailEngine
 		}
 
 	private:
+		std::string name = "unnamed";
 		std::stringstream contents; // makes a lot of sense to be private i know...
+		long int creationTime = 0;
 	};
 
 	class FileSystem : public Publisher, public Subscriber
@@ -60,6 +67,16 @@ namespace detailEngine
 		std::vector<Asset> CollectAssets();
 
 	private:
+		void LoadFile(std::string path);
+		void LoadDir(std::string path); // Loads all the files from a directory
+		std::vector<std::string> DirGetAllFileNamesAbsolute(std::string path);
+		std::string GetPathFileName(std::string path); // returns the file name ONLY
+		std::string GetPathFileType(std::string path); // returns the file type ONLY
+		std::string GetPathFullFilename(std::string path); // returns the file name and its type
+		std::string GetSanitizedPath(std::string path); // Gets SanitizePath() output and assembles it with / in between the words;
+		std::vector<std::string> SanitizePath(std::string path); // splits the path into words
+
+		// All of the code below is garbage
 		void ExecuteAllRequests();
 		void ExecuteRequest(Asset& asset);
 		void ExecuteMessage(Message message);
@@ -67,8 +84,11 @@ namespace detailEngine
 
 		std::vector<File> files;
 
+
+		// All of the code below belongs in a cemetery 
 		std::mutex requestMutex;
 		std::mutex deliverMutex;
+		std::mutex fileioMutex;
 		void SwapRequestBuffers();
 		std::vector<Asset> deliveredAssets;
 		std::vector<Asset> requestedAssets[2];
